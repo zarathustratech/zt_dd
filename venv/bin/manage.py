aa1acc86d39faa16747f63f5d6d00fc2292cefac
Ishan Manandhar <ishan02016@gmail.com>
2019-05-23 17:37:16 +0545

    removed unecessary files

--- venv/bin/manage.py
+++
@@ -1,7 +0,0 @@
-#!"/Users/user/Documents/Projects/Zarathustra Technology/zt_dd/venv/bin/python2.7"
-# EASY-INSTALL-DEV-SCRIPT: 'zt-dd==1.0','manage.py'
-__requires__ = 'zt-dd==1.0'
-__import__('pkg_resources').require('zt-dd==1.0')
-__file__ = '/Users/user/Documents/Projects/Zarathustra Technology/zt_dd/manage.py'
-with open(__file__) as f:
-    exec(compile(f.read(), __file__, 'exec'))
